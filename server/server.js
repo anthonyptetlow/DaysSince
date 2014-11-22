423a8e3e7c2002886dfe6dd6b964fa8477607308
anthonyptetlow <anthonyptetlow@gmail.com>
2014-11-22 16:52:07 +0000

    Refoactored Error Handleing

--- server/server.js
+++ server/server.js
@@ -27,22 +27,8 @@ app.use(function(req,res,next){
 app.use('/api', indexRoutes);
 app.use('/api/user', usersRoutes);
 
-
-//Catch a 404 
-app.use(function (req, res, next) {
-	var err = new Error('Not Found');
-	err.status = 404;
-	next(err);
-;});
-
-app.use(function(err, req, res, next) {
-    res
-    .status(err.status || 500)
-    .json({
-        message: err.message,
-        error: err
-    });
-});
+app.use(require('./modules/notFound'));
+app.use(require('./modules/handleError'));
 
 var server = app.listen(app.get('port'), function() {
   debug('Express server listening on port ' + server.address().port);
