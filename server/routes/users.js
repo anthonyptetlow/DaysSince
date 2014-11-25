b31b69cd8ea09e262869588e4be45cb6d913c3c1
anthonyptetlow <anthonyptetlow@gmail.com>
2014-11-25 22:14:07 +0000

    Added update to the user rest api

--- server/routes/users.js
+++ server/routes/users.js
@@ -25,7 +25,7 @@ router.post('/', function (req, res) {
 	console.log(req.body.username);
 	newUser.username  = req.body.username;
 	newUser.email = req.body.email;
-	newUser.passwordHash;
+	newUser.passwordHash = 'newHash';
 	// newUser.salt
 	newUser.isActivated = false;
 
@@ -35,28 +35,20 @@ router.post('/', function (req, res) {
 	});
 });
 
+router.put('/:id', function (req, res) {
+
+	User.findById(req.params.id, function (error, user) {
+	  	if (error) res.status(500).send(error)
+	  	user.username = req.body.username;
+	  	user.email = req.body.email;
+		user.save(function (error) {
+		  	if (error) res.status(500).send(error)
+			res.status(200).end();
+	  	});
+	})	
 
 
-// router.put('/:id', function (req, res) {
-// 	var db = req.db;
-// 	var collection = db.get('usercollection');
-// 	var userToGet = req.params.id;
-	
-// 	console.log(req.body);
-// 	var userName = req.body.userName;
-// 	var userEmail = req.body.userEmail;
-
-// 	collection.findAndModify({_id: userToGet}, {
-// 		'userName': userName,
-// 		'userEmail': userEmail
-// 	}, function (err, doc) {
-// 		if (err) {
-//             res.send(500, "There was a problem adding the information to the database.");
-// 		} else {
-// 			res.status(200).end();
-// 		}
-// 	});
-// });
+});
 
 router.delete('/:id', function (req, res) {
 	User.findByIdAndRemove(req.params.id, function (error) {
