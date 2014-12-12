bb3b6b6add6fa678550e8b53085932c20773bbd2
anthonyptetlow <anthonyptetlow@gmail.com>
2014-12-12 22:22:53 +0000

    Cleaned out currently redundant files

--- client/src/js/module/controllers/SignUpController.js
+++
@@ -1,9 +0,0 @@
-angular.module('app').controller('SignUpController', ['$scope', function ($scope) {
-	
-	$scope.submitted = false;
-
-	$scope.submit = function () {
-		$scope.submitted = true;
-	};
-
-}]);
