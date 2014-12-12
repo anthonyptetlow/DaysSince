06d6bd4bddfd12c0e63e59c1183b64dc942998cb
anthonyptetlow <anthonyptetlow@gmail.com>
2014-12-12 22:23:42 +0000

    Added twitter authentication in replacement of user authentication

--- server/routes/entry.js
+++ server/routes/entry.js
@@ -3,15 +3,19 @@ var router = express.Router();
 
 var Entry = require('../models/Entry');
 
-router.get('/all/', function (req, res) {
-	Entry.find(function (error, entries) {
-		if (error) res.status(500).send(error);
-		res.json(entries);
-	});
+function getUserId(session) {
+	if (session.passport && session.passport.user && session.passport.user.id) {
+		return session.passport.user.id;		
+	}
+	return undefined;
+}
+
+router.use('/', function (req, res, next) {
+	getUserId(req.session) ? next(): res.status(401);
 });
 
-router.get('/all/:userId', function (req, res) {
-	Entry.find({userId : req.params.userId} ,function (error, entries) {
+router.get('/', function (req, res) {
+	Entry.find({userId : getUserId(req.session)} ,function (error, entries) {
 		if (error) res.status(500).send(error);
 		res.json(entries);
 	});
@@ -20,6 +24,11 @@ router.get('/all/:userId', function (req, res) {
 router.get('/:id', function (req, res) {
 	Entry.findById(req.params.id, function (error, entry) {
 		if (error) res.status(500).send(error);
+
+		if (entry.userId !== getUserId(req.session)) {
+			res.status(404).send("No Entry Found");
+		}
+
 		res.json(entry);
 	});
 });
@@ -27,7 +36,7 @@ router.get('/:id', function (req, res) {
 router.post('/', function (req, res) {
 	var newEntry = new Entry();
 	newEntry.title  = req.body.title;
-	newEntry.userId = req.body.userId;
+	newEntry.userId = getUserId(req.session);
 	newEntry.dates = [new Date()];
 	newEntry.save(function (error) {
 		if (error) res.status(500).send(error);
@@ -37,10 +46,15 @@ router.post('/', function (req, res) {
 
 router.put('/:id', function (req, res) {
 	Entry.findById(req.params.id, function (error, entry) {
-	  	if (error) res.status(500).send(error)
+	  	if (error) res.status(500).send(error);
+		
+		if (entry.userId !== getUserId(req.session)) {
+			res.status(500).send("Unable to update entry");
+		}
+
 	  	entry.title = req.body.title;
 		entry.save(function (error) {
-		  	if (error) res.status(500).send(error)
+		  	if (error) res.status(500).send(error);
 			res.status(200).end();
 	  	});
 	})	
@@ -48,7 +62,11 @@ router.put('/:id', function (req, res) {
 
 router.put('/:id/addEvent', function (req, res) {
 	Entry.findById(req.params.id, function (error, entry) {
-	  	if (error) res.status(500).send(error)
+	  	if (error) res.status(500).send(error);
+
+		if (entry.userId !== getUserId(req.session)) {
+			res.status(500).send("Unable to update entry");
+		}
 	  	entry.dates.push(new Date());
 		entry.save(function (error) {
 		  	if (error) res.status(500).send(error)
@@ -59,8 +77,13 @@ router.put('/:id/addEvent', function (req, res) {
 
 
 router.delete('/:id', function (req, res) {
-	Entry.findByIdAndRemove(req.params.id, function (error) {
+	Entry.findById(req.params.id, function (error, entry) {
 		if (error) res.status(500).send(error);
+
+		if (entry.userId !== getUserId(req.session)) {
+			res.status(500).send("Unable to update entry");
+		}
+		entry.remove();
 		res.status(200).end();
 	})
 });
---
+++ server/routes/twitter.js
@@ -0,0 +1,60 @@
+var express = require('express'),
+	router = express.Router(),
+	passport = require('passport'),
+	TwitterStrategy = require('passport-twitter').Strategy;
+
+var TWITTER_CONSUMER_KEY = "hNCXNBeCfWrv4SjpFFbAT4ZFg";
+var TWITTER_CONSUMER_SECRET = "hBlrslQVAHZWLoEFkI1XWeWTcFndtIcQUBzdbaAPnjpYFhYpJ9";
+
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function(obj, done) {
+  done(null, obj);
+});
+
+passport.use(new TwitterStrategy({
+    consumerKey: TWITTER_CONSUMER_KEY,
+    consumerSecret: TWITTER_CONSUMER_SECRET,
+    callbackURL: "http://127.0.0.1:3000/api/auth/twitter/callback"
+  },
+  function(token, tokenSecret, profile, done) {
+    // asynchronous verification, for effect...
+    // console.log(token);
+    // console.log(tokenSecret);
+    // console.log(profile);
+    process.nextTick(function () {
+      
+      // To keep the example simple, the user's Twitter profile is returned to
+      // represent the logged-in user.  In a typical application, you would want
+      // to associate the Twitter account with a user record in your database,
+      // and return that user instead.
+      return done(null, profile);
+    });
+  }
+));
+
+ // Initialize Passport!  Also use passport.session() middleware, to support
+ // persistent login sessions (recommended).
+router.use(passport.initialize());
+router.use(passport.session());
+
+router.get('/',
+	passport.authenticate('twitter'),
+	function(req, res){
+    // The request will be redirected to Twitter for authentication, so this
+    // function will not be called.
+  	}
+);
+
+// GET /auth/twitter/callback
+//   Use passport.authenticate() as route middleware to authenticate the
+//   request.  If authentication fails, the user will be redirected back to the
+//   login page.  Otherwise, the primary route function function will be called,
+//   which, in this example, will redirect the user to the home page.
+router.get('/callback', 
+  passport.authenticate('twitter', { successRedirect: '/#/app', failureRedirect: '/login' }));
+
+
+module.exports = router;
